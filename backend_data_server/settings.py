3354f4accae975723a2db38f935ad2d11692e4ab
Leonardo Macias <105889369+leodamac@users.noreply.github.com>
2025-07-28 13:13:51 +0000

    Guía 20: Django - Django Rest Framework (DRF)

--- backend_data_server/settings.py
+++ backend_data_server/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "homepage",
+    "rest_framework",
+    "demo_rest_api",
 ]
 
 MIDDLEWARE = [
--- backend_data_server/urls.py
+++ backend_data_server/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-   path("admin/", admin.site.urls),
-   path("", include("homepage.urls")),
+    path("admin/", admin.site.urls),
+    path("", include("homepage.urls")),
+    path("demo/rest/api/", include("demo_rest_api.urls")),
 ]
